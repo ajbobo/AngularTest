f8aae4521772eb90e00e104cd91e162ce8d2e569
aj-bobo <aj.bobo@domo.com>
2017-03-01 08:51:19 -0700

    Added a new parameter to ShowText component

--- src/show-text/show-text.component.js
+++ src/show-text/show-text.component.js
@@ -1,10 +1,15 @@
 angular
     .module("show-text")
     .component("showText", {
-        template: "<div style='background: lightgray; border:1px dashed;'><p>This is a component with text: <b>{{$ctrl.inputText}}</b></p></div>",
+        template: "<div style='background: lightgray; border:1px dashed;'><p>{{$ctrl.displayText + ' '}}<b>{{$ctrl.inputText}}</b></p></div>",
         controller: function () {
+            this.$onInit = function() {
+                if (this.displayText == undefined)
+                    this.displayText = "This is a component with text:"
+            }
         },
         bindings: {
-            inputText: '@'
+            inputText: '@',
+            displayText: '@'
         }
     });
